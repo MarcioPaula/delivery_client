d542714d12fb602ef38524061de0f3e30548951d
MarcioPaula <marciogspaula@gmail.com>
2023-01-09 17:25:28 -0300

    09-01-2023 17:25

--- app/Http/Controllers/clientController.php
+++ app/Http/Controllers/clientController.php
@@ -11,9 +11,9 @@ class clientController extends Controller
 {
     public function cardapio($empresa)
     {
-        $url = 'http://10.1.0.85:8080/api/produtos';
+        $url = 'http://10.1.0.85:8080/api/produtos?api_token=6MWqljFoJ44beKzPRS6fqXT4MJxUDzZQYWG1ZCJmONRyh6QWx1JCZt2SzVlU';
         $client = new Client();
-        $res = $client->get($url);
+        $res = $client->request('GET',$url);
         $produtos = json_decode($res->getBody(),true);
 
         return view('pages.cardapio',compact('produtos','empresa'));
